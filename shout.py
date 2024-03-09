f132f0ca9a132ea6e8f2ec21dd609790b71644cc
tigger <tigger04@gmail.com>
2024-03-09 13:00:37 +0000

    nospoon:acp

--- shout.py
+++ shout.py
@@ -5,6 +5,9 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFontMetrics
 from PyQt5.QtCore import QTimer
+from PyQt5.QtGui import QFontDatabase
+from PyQt5.QtWidgets import QGraphicsOpacityEffect
+from PyQt5.QtCore import QPropertyAnimation
 
 # aliases
 ceil = math.ceil
@@ -14,8 +17,12 @@ floor = math.floor
 max_screen_width_ratio = 0.8
 max_screen_height_ratio = 0.8
 text_margin = 20
-floating_window = False
+floating_window = True
 app_name = "shout!"
+fade_in_duration = 1000  # TODO
+fade_out_duration = 1000  # TODO
+auto_close_duration = 5000  # TODO
+window_opacity = 0.8
 
 class ShoutText(QTextEdit):
     def __init__(self, parent=None):
@@ -23,15 +30,25 @@ class ShoutText(QTextEdit):
 
         self.window = parent
 
-        self.font = QFont("Sudoers", 24)  # Set the font to "Sudoers" and size to 24 points
+        # self.font = QFontDatabase.systemFont(
+        #     QFontDatabase.FixedFont
+        # )  # Set the font to system's monospaced font
+        # self.setFontPointSize(24)  # Set the font size to 18 points
+
+        self.font = QFont(
+            "Sudoers", 24
+        )  # Set the font to a generic monospaced font and size to 24 points
         self.setFont(self.font)
-        self.setFontPointSize(24)  # Set the font size to 18 points
         self.document().setDocumentMargin(text_margin)
         self.setReadOnly(True)
 
         if floating_window:
             # Set the window to stay on top of all others
             window.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+
+        if auto_close_duration > 0:
+            print("setting auto close duration to", auto_close_duration, "ms")
+            QTimer.singleShot(auto_close_duration, window.close)
 
     def setText(self, text):
         super(ShoutText, self).setText(text.rstrip('\n'))
@@ -49,7 +66,7 @@ class ShoutText(QTextEdit):
         # text = self.toPlainText().rstrip('\n') # Remove trailing newlines
         lines = self.toPlainText().split('\n')
         font_metrics = QFontMetrics(self.font)
-        max_width = max(font_metrics.width(line) for line in lines)
+        max_width = max(font_metrics.horizontalAdvance(line) for line in lines)
 
         # Add the document margin to the width
         margin = doc().documentMargin()
@@ -57,7 +74,7 @@ class ShoutText(QTextEdit):
 
         screen_width_max = (max_screen_width_ratio * app.desktop().screenGeometry().width())  # Get the screen width
         screen_height_max = (max_screen_height_ratio * app.desktop().screenGeometry().height())  # Get the screen height
-    
+
         doc_height = doc().size().height()
 
         self.window.setFixedHeight(int(min(ceil(doc_height), screen_height_max)))
@@ -85,7 +102,9 @@ signal.signal(signal.SIGINT, signal.SIG_DFL) # Enable Ctrl-C to close the applic
 
 # Create a QMainWindow instance
 window = QMainWindow()
-window.setWindowOpacity(0.8)  # Set the opacity to 0.8 (1.0 is fully opaque, 0.0 is fully transparent)
+window.setWindowOpacity(
+    window_opacity
+)  # Set the opacity to 0.8 (1.0 is fully opaque, 0.0 is fully transparent)
 
 # Make the window borderless
 window.setWindowFlags(Qt.FramelessWindowHint)
